fd332b58f86501fb37ad8b680e25f44fd38418d7
abdubash04 <165327233+abdubash04@users.noreply.github.com>
2024-05-30 10:22:36 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+    'index.html',
+    'product.js',
+    'petstore.webmanifest',
+    'images/img.png'
+    
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install ');
+    e.waituntill(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
+
+});
+
+self.addEventListener('fetch', function (e){
+    e.respondWith(
+        caches.match(e.request).then(function (r){
+            return r || fetch(e.request).then(function(response){
+                return caches.open(cacheName).then(function (cache){
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
